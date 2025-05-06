fdaec0e3888bc783ae573af3e169e8cd27e71b93
DeadlyWhiskers <133860751+DeadlyWhiskers@users.noreply.github.com>
2025-05-06 21:36:49 +0300

    Add layout to the settings page

--- src/Extension/pages/SettingsPage/SettingsPage.tsx
+++ src/Extension/pages/SettingsPage/SettingsPage.tsx
@@ -1,6 +1,37 @@
+import { useState } from 'react';
+import Slider from '../../widgets/Slider';
+import './SettingsPage.css'
+
 const SettingsPage = () => {
+
+    const [showRemainingTime, setShowRemainingTime] = useState(false);
+    const [showCooldownTime, setShowCooldownTime] = useState(false);
+
     return <div className='list-layout'>
-        Settings page
+        <div className="content-block content-block--large">
+            <div className="input-line">
+                <span className="title">
+                    Home page:
+                </span>
+                <input type="text" className="input-field"placeholder="google.com"/>
+            </div>
+        </div>
+        <div className="content-block content-block--large">
+            <div className="input-line">
+                <span className="title">
+                    Show remaining time
+                </span>
+                <Slider id={'remainingTimeSwitch'} checked={showRemainingTime} onChange={() => (setShowRemainingTime(!showRemainingTime))}/>
+            </div>
+        </div>
+        <div className="content-block content-block--large">
+            <div className="input-line">
+                <span className="title">
+                    Show cooldown time
+                </span>
+                <Slider id={'cooldownTimeSwitch'} checked={showCooldownTime} onChange={() => (setShowCooldownTime(!showCooldownTime))}/>
+            </div>
+        </div>
     </div>;
 };
 
