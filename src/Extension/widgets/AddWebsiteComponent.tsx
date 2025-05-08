31da635bb4d63cf9743811550f85ba7ee9631f6a
DeadlyWhiskers <133860751+DeadlyWhiskers@users.noreply.github.com>
2025-05-08 18:24:45 +0300

    Add form validation

--- src/Extension/widgets/AddWebsiteComponent.tsx
+++ src/Extension/widgets/AddWebsiteComponent.tsx
@@ -4,42 +4,93 @@ import CrossIcon from './assets/CrossIcon.svg'
 import TickIcon from './assets/TickIcon.svg'
 import PlusIcon from './assets/PlusIcon.svg'
 import './AddWebsiteComponent.css'
-import { useState } from 'react';
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const timeToSeconds = (time: string): number => {
+    let seconds = 0;
+    const numbers = time.split(':').reverse();
+    numbers.map((num, i) => {
+        seconds += +num * (60 ** i);
+    })
+    return seconds;
+}
+
+const formSchema = z.object({
+    name: z.string().min(1,'This field is required'),
+    address: z.string().url().min(1,'This field is required'),
+    limitTime: z.string({required_error: 'This field is required'}).time({precision: 0}).min(1).max(8),
+    cooldownTime: z.string({required_error: 'This field is required'}).time({precision: 0}).min(1).max(8),
+    formError: z.string().optional()
+})
+.refine((data) => timeToSeconds(data.limitTime) <= timeToSeconds(data.cooldownTime), {message: 'Cooldown time is less than a limit time', path: ['formError']})
+
+type FormFields = z.infer<typeof formSchema>
 
 const AddWebsiteComponent = () => {
+    
+    console.log(timeToSeconds('20:44:15'))
+
     const [addActive, setAddActive] = useState(0);
+
+    const handleCancel = () => {
+        setAddActive(0);
+        reset();
+    }
+
+    const {register,  handleSubmit, setError, formState: {errors, isSubmitting}, reset} = useForm<FormFields>({resolver: zodResolver(formSchema)});
+
+    const onSubmit: SubmitHandler<FormFields> = async (data) => {
+        try{
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+        catch(e){
+            console.log(e)
+            setError('root', {message: 'Something went wrong'});
+        }
+        console.log('submitted', data);
+    }
+
     return (
         <div className="add-website-component">
             {!addActive ? <button className="button button--add-website" onClick={() => setAddActive(1)}>
                 <img src={PlusIcon} alt="plus icon" />
             </button>
                 :
-                <div className='content-block content-block--large'>
+                <form className='content-block content-block--large' onSubmit={handleSubmit(onSubmit)}>
                     <div className="input-line">
                         <span className='title'>Display name:</span>
-                        <input type="text" className='input-field' />
+                        <input type="text" className='input-field' {...register('name', {required: true})}/>
                     </div>
+                    {errors.name && <div className='error'> {errors.name.message} </div>}
                     <div className="input-line">
                         <span className='title'>Url:</span>
-                        <input type="text" className='input-field' />
+                        <input type="text" className='input-field' {...register('address')}/>
                     </div>
+                    {errors.address && <div className='error'> {errors.address.message} </div>}
                     <div className="horizontal-spread-area">
                         <div className="add-limits-time-area">
                             <div className="input-line">
                                 <span className='title'>Limit:</span>
-                                <input type="text" className='input-field input-field--short' maxLength={8} minLength={2}/>
+                                <input type="text" placeholder='00:00:00' className='input-field input-field--short' maxLength={8} minLength={2} {...register('limitTime')}/>
                             </div>
+                            {errors.limitTime && <div className='error'> {errors.limitTime.message} </div>}
                             <div className="input-line">
                                 <span className='title'>Cooldown:</span>
-                                <input type="text" className='input-field input-field--short' maxLength={8} minLength={2}/>
+                                <input type="text" placeholder='00:00:00' className='input-field input-field--short' maxLength={8} minLength={2} {...register('cooldownTime')}/>
                             </div>
+                            {errors.cooldownTime && <div className='error'> {errors.cooldownTime.message} </div>}
                         </div>
                         <div className="buttons-area">
-                            <IconButton image={CrossIcon} color='var(--negative-color)' onClick={() => setAddActive(0)}/>
-                            <IconButton image={TickIcon} color='var(--positive-color)' onClick={() => setAddActive(0)}/>
+                            <IconButton image={CrossIcon} color='var(--negative-color)' onClick={() => handleCancel()} disabled={isSubmitting}/>
+                            <IconButton image={TickIcon} color='var(--positive-color)' type='submit' disabled={isSubmitting}/>
                         </div>
                     </div>
-                </div>}
+                    {errors.formError && <div className='error'> {errors.formError.message} </div>}
+                    {errors.root && <div className='error'> {errors.root.message} </div>}
+                </form>}
         </div>
     );
 }
--- src/Extension/widgets/IconButton.tsx
+++ src/Extension/widgets/IconButton.tsx
@@ -3,13 +3,15 @@ import './IconButton.css'
 type ButtonProps = {
     image: string,
     color: string,
-    onClick?: () => void,
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void,
+    type?: "button" | "submit" | "reset",
+    disabled?: false | true
 }
 
 const IconButton = (props: ButtonProps) => {
     return(
-        <button className='button button--icon-button' style={{backgroundColor: props?.color}} onClick={() => props.onClick && props.onClick()}>
-            <img src={props?.image} alt="button icon" />
+        <button className='button button--icon-button' style={{backgroundColor: props?.color}} onClick={(e) => !!props.onClick && props.onClick(e)} type={props.type || 'button'} disabled={props.disabled || false}>
+            <img src={props?.image} alt="button icon"/>
         </button>
     );
 };
